284971a5edae8cbea9ed1163fc0f2aef30b24142
yourUsername <yourEmail@domain.com>
2020-12-06 11:56:28 -0500

    ff

---
+++ src/Earth.java
@@ -0,0 +1,15 @@
+public class Earth {
+
+public static void main(String arg[]){
+
+    Human tom;
+   tom = new Human();
+    tom.name = "Tom cat";
+    tom.age = 5;
+    tom.heightInInches = 72;
+    tom.eyeColor = "brown";
+    tom.speak();
+}
+
+
+}
---
+++ src/Human.java
@@ -0,0 +1,53 @@
+public class Human {
+    String name;
+   int age;
+   int heightInInches;
+   String eyeColor;
+   // String subject;
+  // String food;
+
+    // constructor classd
+  public Human(){
+
+  }
+  //Method name speak
+   public void speak(){
+
+       System.out.println( "Hello, my name is" + name);
+       System.out.println( "I am" + heightInInches+ "inches tall");
+       System.out.println( "I am" +age+ "years old" );
+       System.out.println( "My eye color is" + eyeColor );
+   // System.out.println( "This is my final:"+ subject);
+   }
+   // Create a method name called eat
+
+    public void eat(){
+
+       System.out.println( "eating....");
+   }
+  public void walk(){
+       System.out.println( "walking..." );
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
